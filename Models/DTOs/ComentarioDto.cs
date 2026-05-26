c87819cbb4ca3f601764671bdca20b7b3d286e3a
Suporte TI <suporteti@ImobiliariaLopes.local>
2026-05-26 15:23:14 -0300

    Estruturando, otimizando e comentando no codigo

--- Models/DTOs/ComentarioDto.cs
+++ Models/DTOs/ComentarioDto.cs
@@ -4,7 +4,6 @@ namespace TremBomApi.Models.DTOs
 {
     public class ComentarioDto
     {
-        [Required(ErrorMessage = "O conteúdo do comentário é obrigatório sô!")]
         [MaxLength(100, ErrorMessage = "O comentário não pode passar de 100 caracteres.")]
         public required string Texto { get; set; }
     }
